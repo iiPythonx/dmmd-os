2de48b6e82585747b97a92fc37f56ca820cc5fc1
iiPython <ben@iipython.dev>
2025-01-16 23:29:14 -0600

    slight fixes

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,10 +13,10 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 # Handle loading apps
-apps_path = Path("apps")
+apps_path = Path(__file__).parents[1] / "apps"
 def generate_app_data() -> dict:
     apps = {}
-    for file in apps_path.rglob("**"):
+    for file in apps_path.rglob("*"):
         if not file.is_file():
             continue
 
@@ -31,16 +31,20 @@ def generate_app_data() -> dict:
 
         apps[str(file.relative_to(apps_path).with_suffix(""))] = directives | {"html": "\n".join(processed_content)}
 
+    print("[+] Regenerated app data")
     return apps
-
 
 # Routing
 app = FastAPI(openapi_url = None)
 app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
 
+app.state.cached_apps = None
+if os.getenv("ENV") == "prod":
+    app.state.cached_apps = generate_app_data()
+
 @app.get("/apps.json")
 async def route_apps() -> JSONResponse:
-    return JSONResponse(generate_app_data())
+    return JSONResponse(app.state.cached_apps or generate_app_data())
 
 # Proxying
 session = Session()
