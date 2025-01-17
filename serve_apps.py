45ed3fbf41aee6ab9e0400ee88dec81daabe7071
iiPython <ben@iipython.dev>
2025-01-16 19:33:07 -0600

    basically, its internet explorer but like v1

--- serve_apps.py
+++ serve_apps.py
@@ -1,11 +1,19 @@
 # Copyright (c) 2025 iiPython
 
 # Modules
+from math import atan
+import re
+import base64
 from pathlib import Path
+from binascii import Error
 
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from requests import Session
+from bs4 import BeautifulSoup
+
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from requests.exceptions import SSLError
 
 # Handle loading apps
 apps_path = Path("apps")
@@ -28,6 +36,7 @@ def generate_app_data() -> dict:
 
     return apps
 
+
 # Routing
 app = FastAPI(openapi_url = None)
 app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
@@ -35,3 +44,53 @@ app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = Tr
 @app.get("/apps.json")
 async def route_apps() -> JSONResponse:
     return JSONResponse(generate_app_data())
+
+# Proxying
+session = Session()
+CSS_URL_REGEX = re.compile(r"url\(['\"]?([^\"')]+)['\"]?\)")
+
+@app.get("/ie/{url:str}", response_model = None)
+async def proxy_ie_url(url: str) -> Response | HTMLResponse:
+    try:
+        response = session.get(base64.b64decode(url.encode()).decode())
+        domain = "/".join(response.url.split("/")[:3])
+
+        content = response.content
+
+        def encode(url: str) -> str:
+            return base64.b64encode(url.encode()).decode()
+
+        def fix_url(url: str) -> str:
+            url = f"https://{url}" if url[:2] == "//" else url
+            return url if url[0] != "/" else f"{domain}{url}"
+
+        content_type = (response.headers.get("Content-Type") or "").split(";")[0]
+        if content_type == "text/css":
+            for item in CSS_URL_REGEX.findall(response.text):
+                content = content.replace(item.encode(), encode(fix_url(item)).encode())
+
+        if content_type == "text/html":
+            html = BeautifulSoup(response.text, "html.parser")
+            def replace(tag: str, attrib: str) -> None:
+                for item in html.find_all(tag):
+                    try:
+                        item[attrib] = f"http://localhost:8001/ie/{encode(fix_url(item[attrib]))}"
+
+                    except KeyError:
+                        pass
+
+            for (tag, attrib) in [("a", "href"), ("link", "href"), ("script", "src"), ("img", "src")]:
+                replace(tag, attrib)
+
+            return HTMLResponse(str(html), headers = {"X-New-Url": response.url})
+
+        return Response(
+            content,
+            headers = {"Content-Type": response.headers["Content-Type"], "X-New-Url": response.url}
+        )
+
+    except Error:
+        return HTMLResponse("<p>The specified URL is invalid.</p>")
+
+    except SSLError:
+        return HTMLResponse("<p>SSL error when attempting to connect.</p>")
